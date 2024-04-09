53e15a04dff7da416ecde6ab70376241dc5c9c57
Dawson <127239947+Dawson02@users.noreply.github.com>
2024-04-09 14:07:58 -0230

    Complete frontend

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import Search from "./pages/Search";
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
